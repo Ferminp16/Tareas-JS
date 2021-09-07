c0f2433c59b84a47847310ea3a1d8a70e02c8111
Fermin Perez <ferminperez_@hotmail.com>
2021-09-07 18:17:16 -0300

    tarea 1

---
+++ main.js
@@ -0,0 +1,14 @@
+let nombre = prompt("Ingrese su nombre")
+let apellido = prompt("Ingrese su apellido")
+let remerasCelestes = parseInt(prompt("Cuantas remeras celestes quiere?"))
+let remerasBlancas = parseInt(prompt("Cuantas remeras blancas quiere?"))
+
+
+let totalRemeras = remerasBlancas + remerasCelestes
+
+
+alert ("Nombre:" + " " +  nombre  +  "\nApellido:" + " " + apellido + "\nCompraste" + " " + totalRemeras + " " + "remeras" )
+
+
+
+
