20b454656c3e65939ee02bd3d8b92697a787eb2e
Fermin Perez <ferminperez_@hotmail.com>
2021-09-30 21:40:53 -0300

    tarea - interactuar con html - Fermin Perez

--- js/main.js
+++ js/main.js
@@ -1,75 +1,39 @@
+alert("Bienvenido a EVAHome")
 
-// Esta es la tarea 3 y 4 //
+for (const producto of productos){
+    let container = document.createElement("div");
+    container.innerHTML = `<h3>Producto: ${producto.name}</h3>
+                           <p>Precio: ${producto.price}</p>
+                           <p>Stock: ${producto.stock}</p>
+                           <p>Codico: ${producto.id}</p>`
+document.body.appendChild(container)
 
-// alert("Bienvenido")
+container.classList.add("containerProducto")
 
-
-// do {
-
-//     let producto = parseInt(prompt(menu))
-//     if (producto === 5) break
-//     let cantidad = parseInt(prompt(selecCantidad))
-
-
-//     addToCart(producto, cantidad)
-//     resp = prompt("¿Desea agregar otro producto al carrito? SI / NO")
-
-
-// } while (resp === "SI");
-
-// if (total > 0) {
-
-//     alert(`El total de su compra es de ${total}`)
-// }
-
-// alert("Gracias por su compra")
-
-
-
-
-//Esta es la tarea de objetos//
-
-class futbolista{
-    constructor(nombre, numero, piernaHabil, altura, peso){
-        this.nombre = nombre
-        this.numero = numero
-        this.piernaHabil = piernaHabil
-        this.altura = altura
-        this.peso = peso
-    }
-
-    patea() {
-        return ` ${this.nombre} patea`;
-    }
-
-    salta(){
-        return ` ${this.nombre} salta`;
-    }
-
-    cabecea(){
-        return  `${this.nombre} cabecea`
-    }
-
-    toString(){
-        return `Nombre: ${this.nombre}
-                Numero: ${this.numero}
-                Pierna habil: ${this.piernaHabil}
-                Altura: ${this.altura} cm
-                Peso: ${this.peso} kg
-        `
-    }
 }
-   
 
-const nombre = prompt(`Ingrese el nombre su jugador`)
-const numero = parseInt(prompt(`Ingrese el numero de ${nombre}`))
-const piernaHabil = prompt(`Ingrese la pierna habil de ${nombre}`)
-const altura = parseInt(prompt(`Ingrese la altura de ${nombre} en cm`))
-const peso = parseInt(prompt(`Ingrese el peso de ${nombre} en kg`))
+do {
+    
+    let option = mostrarMenu();
+    if (option === productos.length + 1) break
+    let cantidad = parseInt(prompt(selecCantidad))
+    
+    
+    addToCart(option, cantidad)
+    
+    resp = prompt("¿Desea agregar otro producto al carrito? SI / NO")
+    resp = resp.toUpperCase()
+    
+} while (resp === "SI");
 
-const jugador1 = new futbolista(nombre, numero, piernaHabil, altura, peso)
+if (total > 0) {
+    
+    alert(`El total de su compra es de $${total}`)
+}
+
+alert("Gracias por su compra")
 
 
-alert (jugador1.toString())
-console.log (jugador1.toString())
+
+
 
--- js/variables.js
+++ js/variables.js
@@ -1,18 +1,15 @@
-// const menu = `Elija el numero del prducto que desea:
-// 1 - Cortinas 
-// 2 - Sabanas 
-// 3 - Acolchado 
-// 4 - Almoahada
-// 5 - Salir
-// `;
+const selecCantidad = "¿Que cantidad desea comprar?"
 
-// const selecCantidad = "¿Que cantidad desea comprar?"
+let productos = [
+    { id: 1, name: "Cortinas", price: 1000, stock: 5 },
+    { id: 2, name: "Sabanas", price: 1500, stock: 8 },
+    { id: 3, name: "Acolchados", price: 3000, stock: 3 },
+    { id: 4, name: "Almohada", price: 500, stock: 10 },
+    { id: 5, name: "Toalla", price: 400, stock: 15 },
+    { id: 6, name: "Repasador", price: 300, stock: 20 },
+];
 
-// const precioCortinas = 1000
-// const precioSabanas = 1500
-// const precioAcolchado = 3000
-// const precioAlmohada = 500
 
-// let total = 0
+let total = 0
 
-// let resp = "SI"
+let resp = "SI"
